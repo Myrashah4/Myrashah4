961737fc57a1ae1bb0091e7d7add3275f6fb4574
Myra <159658564+Myrashah4@users.noreply.github.com>
2024-03-04 01:04:58 +0530

    Update CO2Emission.py

--- CO2Emission.py
+++ CO2Emission.py
@@ -1,3 +1,7 @@
+
+#Read CO2 emission file and visualize the data in at least 2 different type of charts of your choice, then save them in 3 different picture formats of your choice. The graphs should make logical sense 
+#and one should be able to read them.
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
